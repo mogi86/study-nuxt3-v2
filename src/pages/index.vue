eb8b04c0bd608d28caab3d90d9017d85ffca2c5e
mogi86 <nobuyuki.mogi@gmail.com>
2022-08-26 14:01:56 +0900

    display process

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,20 @@
 <template>
   <h1>Top Page</h1>
+  <div>
+    Process is {{ currentProcess }}
+  </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: "index"
-}
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  setup () {
+    return {
+      currentProcess: process.client ? 'client' : 'server'
+    }
+  }
+})
 </script>
 
 <style scoped>
