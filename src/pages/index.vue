1b481b13b30175e88b54aeba64dff48a23f59823
mogi86 <nobuyuki.mogi@gmail.com>
2022-11-20 19:29:52 +0900

    nest async function sample

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,30 +2,13 @@
   <h1>Index Page!</h1>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import {set} from "~/service/SetCookie";
-import {defineComponent} from "vue";
-import {useCookie} from "#app";
 
-export default defineComponent({
-  setup () {
-    console.log('step-1')
-
-    async function hoge () {
-      console.log('step-2')
-      await set()
-      console.log('step-3')
-    }
-    hoge()
-    console.log('step-4')
-
-    console.log(useCookie('hoge').value)
-
-    return {
-      hoge
-    }
-  }
-})
+async function hoge () {
+  set()
+}
+await hoge()
 </script>
 
 <style scoped>
--- src/service/SetCookie.ts
+++ src/service/SetCookie.ts
@@ -1,11 +1,19 @@
 import {useCookie} from "#app";
 
-async function set () {
+async function setPrivate () {
+    useCookie('hoge', {
+        secure: true
+    }).value = 'private'
+}
+
+export async function set () {
     useCookie('hoge', {
         secure: true
     }).value = '123'
-}
 
-export {
-    set
+    await setPrivate()
+
+    useCookie('hoge', {
+        secure: true
+    }).value = '777'
 }
