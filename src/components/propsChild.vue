aaca061fdfc2cb522362af6ef5f13fee83bb4630
mogi86 <nobuyuki.mogi@gmail.com>
2023-01-01 20:17:15 +0900

    add sample code for defineProps

---
+++ src/components/propsChild.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>Props Child</h2>
+  <div>props.child: {{ props.child }}</div>
+  <div>props.child2: {{ child2 }}</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  child: {
+    type: String,
+    default: 'default'
+  },
+  child2: {
+    type: String,
+    default: 'default'
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/props.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Props</h1>
+  <props-child child="updated" child2="updated"></props-child>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
