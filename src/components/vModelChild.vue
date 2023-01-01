d13608a2c6160a6d18320d2da9275b5dae10901d
mogi86 <nobuyuki.mogi@gmail.com>
2023-01-01 13:25:06 +0900

    add sample code for v-model

---
+++ src/components/vModelChild.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>v-model Child</h2>
+  <div>
+    <input
+        type="text"
+        :value="child"
+        @input="$emit('update:child', $event.target.value)"
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  child: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/v_model.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>v-model</h1>
+  <div>
+    <input type="text" v-model="content">
+  </div>
+  <div>
+    <button @click="change">Change</button>
+  </div>
+  <div>
+    <v-model-child v-model:child="content" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from "@vue/runtime-core";
+import VModelChild from "~/components/vModelChild.vue";
+
+const content = ref('')
+
+function change () {
+  content.value = 'changed'
+}
+</script>
+
+<style scoped>
+
+</style>
